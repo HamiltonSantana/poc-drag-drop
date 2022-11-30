eaa898fcf9e509ce3e5af03c8317545b47c55ce5
Hamilton <hamiltonsds@examroom.ai>
2022-11-30 13:37:15 -0300

    fix(fixing buidl) - fix build and add new behaviour to richtext

--- src/app/rich/components/rich/rich.component.ts
+++ src/app/rich/components/rich/rich.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewContainerRef, ViewEncapsulation } from '@angular/core';
 import { ToolbarModule } from '@syncfusion/ej2-angular-navigations';
 import { HtmlEditorService, ImageService, LinkService, RichTextEditorComponent, ToolbarService } from '@syncfusion/ej2-angular-richtexteditor';
+import { DragComponent } from 'src/app/drag/drag/drag.component';
 
 @Component({
   selector: 'app-rich',
@@ -10,7 +11,7 @@ import { HtmlEditorService, ImageService, LinkService, RichTextEditorComponent, 
   encapsulation: ViewEncapsulation.None,
 })
 export class RichComponent implements OnInit {
-  @ViewChild('richEditorComponent') richEditor : RichTextEditorComponent | undefined
+  @ViewChild('richEditor') richEditor! : RichTextEditorComponent;
 
   public tools: ToolbarModule = {
     items: ['Bold', 'Italic', 'Underline', 'StrikeThrough',
@@ -22,7 +23,7 @@ export class RichComponent implements OnInit {
         'SourceCode', 'FullScreen', '|', 'Undo', 'Redo']
 };
   
-  constructor() { }
+  constructor(private viewRef : ViewContainerRef) { }
 
   ngOnInit(): void {
     
@@ -30,10 +31,16 @@ export class RichComponent implements OnInit {
 
   addClozer()
   {
-    let element = document.createElement('p')
+    const drag = this.viewRef.createComponent(DragComponent)
+    let element = document.createElement('button')
     element.innerHTML ='teste';
     element.classList.add('gay');
-    document.getElementById('richEditor')?.appendChild(element);
+    const textarea = this.richEditor.element.querySelector('.e-content')
+    textarea?.append(document.createTextNode('\u00A0'));
+    textarea?.append(drag.location.nativeElement);
+    textarea?.append(document.createTextNode('\u00A0'));
     console.log(element)
+    const drag2 = this.viewRef.createComponent(DragComponent)
+    document.getRootNode().parentElement?.append(drag2.location.nativeElement)
   }
 }
